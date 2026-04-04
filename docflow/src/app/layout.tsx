d3fd827ec3e80e21f6fe04d5fd7fa284d683d921
chriscs0625 <IT23254398@MY.SLIIT.LK>
2026-04-04 21:17:11 +0530

    tested

--- docflow/src/app/layout.tsx
+++ docflow/src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={geist.className}>
+      <body className={inter.className}>
         {children}
       </body>
     </html>
